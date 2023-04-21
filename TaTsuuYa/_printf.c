4247942636942fa1eb135d29dc86d4b622c5e44d
TaTsuuYa <taoufik02yahyaoui@gmail.com>
2023-04-21 11:40:43 -0700

    0. I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life
    handling: c, s and %

---
+++ TaTsuuYa/_printf.c
@@ -0,0 +1,72 @@
+#include "main.h"
+
+/**
+ * _printf - mimics printf
+ * @format: string pointing to thr formatting of
+ * printed string
+ *
+ * Return: int number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+	while (format[i] != 0)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			handle_format(format[i], &args);
+		}
+		else
+		{
+			_putchar(format[i]);
+		}
+		i++;
+	}
+	va_end(args);
+
+	return (i);
+}
+
+/**
+ * handle_format - handles the % sign
+ * @f: formatting specifier
+ * @args: pointer to args;
+ *
+ * Return: void 
+ */
+
+void handle_format(char f, va_list *args)
+{
+	char *s;
+
+	switch (f)
+	{
+		case 'c':
+			_putchar(va_arg(*args, int));
+			break;
+		case 's':
+			s = va_arg(*args, char *);
+			while (*s != 0)
+			{
+				_putchar(*s);
+				s++;
+			}
+			break;
+		case '%':
+			_putchar('%');
+			break;
+		default:
+			_putchar('%');
+			_putchar(f);
+			break;
+	}
+}
+
