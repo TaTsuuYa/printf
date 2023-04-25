5731467552e37a262ce6c34d834e4f6ded4765c8
zakelh6 <zakaria.elhajoui1@gmail.com>
2023-04-25 16:38:19 -0700

    add function to print nb to hex

---
+++ _puthex.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puthex - print  number to hex
+ * @nb: number to be printed
+ * @c: the flag
+ */
+int _puthex(unsigned int nb, char c)
+{
+	int len;
+
+	len = 0;
+	if (nb > 15)
+		len += _puthex(nb / 16, c);
+	if (c == 'x')
+		len += write(1, &"0123456789abcdef"[nb % 16], 1);
+	else
+		len += write(1, &"0123456789ABCDEF"[nb % 16], 1);
+	return (len);
+}
