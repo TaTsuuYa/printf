ed7a3e9be5b36df86fc1e5fe2e408cf53ecb2ba9
zakelh6 <zakaria.elhajoui1@gmail.com>
2023-04-25 17:21:34 -0700

    Task 5

---
+++ _putS.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _putS - A function that printd a string to stdout
+ * @str: the string to be printed
+ * Return: the numbers of printed characters
+ */
+int _putS(char *str)
+{
+	int len = 0;
+
+	if (!str)
+		return (_puts("(null)"));
+	while (*str != '\0')
+	{
+		if ((*str > 0 && *str < 32) || *str >= 127)
+		{
+			len += _puts("\\x0");
+			len += _puthex(*str, 'X');
+		}
+		else
+		{
+			len += _putchar(*str);
+		}
+		str++;
+	}
+	return (len);
+}
